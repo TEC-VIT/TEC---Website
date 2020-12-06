06fe3d0461571d497f5f8d1278cf4228d4de3d7f
Yashwanth Kolli <yashwanthkolli3271gmail.com>
2020-12-06 15:25:49 +0530

    NavBar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,37 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-import { Link, useHistory } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { scroller } from 'react-scroll'
 
 import './NavBar.Styles.scss'
 
 const NavBar = () => {
+    const location = useLocation()
+
+    useEffect(()=> {
+        if (location.hash) {
+            scrollTo(location.hash.slice(1))
+        } else {
+        window.scrollTo({top:0,left:0, behavior: "smooth"})
+        }
+    }, [location,])
+
     const scrollTo = (section) => {
         scroller.scrollTo(section, {
             duration: 800,
             delay: 0,
+            offset: -100,
             smooth: 'easeInOutQuart'
         })
     }
 
-    let history = useHistory()
-    const navigateToHomeAndScroll = (section) => {
-        history.push('/')
-        scrollTo(section)
-    }
+    const [visible, setVisible] = useState(true)
+
+    var body = document.getElementById('root')
+    body.addEventListener('wheel', (e) => {
+        e.deltaY > 0 ? setVisible(false) : setVisible(true)
+    })
 
     return (
-        <nav className='navbar'>
-            <div className='nav-logo'>
+        <nav className='navbar' style={{opacity: `${visible ? '1' : '0'}`, top: `${visible ? '0' : '-7rem'}`}}>
+            <Link className='nav-logo' to='/'>
 
-            </div>
+            </Link>
             <div className='nav-links'>
                 <Link className='nav-option' to='/'>Home</Link>
-                <span className='nav-option' onClick={() => navigateToHomeAndScroll('about-us-section')}>About Us</span>
+                <Link className='nav-option' to="/#about-us-section">About Us</Link>
                 <Link className='nav-option' to='/events'>Events</Link>
                 <Link className='nav-option' to='/projects'>Projects</Link>
-                <span className='nav-option' onClick={() => scrollTo('board-section')}>Board</span>
-                <span className='nav-option' onClick={() => scrollTo('contact-us-section')}>Contact Us</span>
+                <Link className='nav-option' to='/#board-section'>Board</Link>
+                <Link className='nav-option' to='/#contact-us-section'>Contact Us</Link>
             </div>
         </nav>
     )
