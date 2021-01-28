ea05fe255d1ada79e5afdd09a19f94f08758c6b1
Ankita-Mandal <anki.mandal@gmail.com>
2021-01-28 10:58:14 +0530

    flip annimation on event card

--- src/Components/EventCard/EventCard.js
+++ src/Components/EventCard/EventCard.js
@@ -5,8 +5,19 @@ import './EventCard.Styles.scss'
 const EventCard = ({event}) => {
     return (
         <div className='event-card'>
-            <img className='event-img' src={event.poster} alt='event_poster' />
-            <p className='event-description'>{event.body}</p>
+            <article className="card">
+                <div className='flip-card-front'>
+                    <picture className='card-background'>
+                        <img className='event-img' src={event.poster} alt='event_poster' />
+                    </picture>                    
+                    <h1 className='event-title'>{event.title} </h1>
+                </div>
+                <div className='flip-card-back'>
+                    <p className='event-description'>{event.body}</p>
+                </div>
+            </article>
+            
+            
         </div>
     )
 }
