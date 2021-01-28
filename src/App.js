86bede57da0a5f01ec53c589dc5e6b7d63eed182
Yashwanth Kolli <yashwanthkolli3271gmail.com>
2021-01-28 16:08:19 +0530

    Loading animation

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import { Route, Switch } from 'react-router-dom';
 
 import './App.scss';
@@ -5,16 +6,30 @@ import './App.scss';
 import HomePage from './Pages/HomePage/HomePage';
 import EventsPage from './Pages/EventsPage/EventsPage';
 import { Footer } from './Components/Footer/Footer';
+import Arduino from './Components/Loader/Arduino';
+
+
 
 function App() {
+  const [loading, setLoading] = useState('loading')
+  document.onreadystatechange = () => {
+    setLoading(document.readyState)
+  }
   return (
-    <div className="App">
+    <React.Fragment>
+    <div style={{display: `${loading !== 'complete' ? 'inline' : 'none' }`}} className='loader'>
+      <p>Uploading Code...</p>
+      <Arduino />
+      <div className='spinner'></div>
+    </div>
+    <div style={{visibility: `${loading !== 'complete' ? 'hidden' : 'visible' }`}} className="App">
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/events' component={EventsPage} />
       </Switch>
       <Footer />
     </div>
+    </React.Fragment>
   );
 }
 
