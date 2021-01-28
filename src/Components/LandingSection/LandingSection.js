fc9100571acb27decc05b6aa6ece929911a2d00f
Yashwanth Kolli <yashwanthkolli3271gmail.com>
2021-01-28 12:29:03 +0530

    Styling changes

--- src/Components/LandingSection/LandingSection.js
+++ src/Components/LandingSection/LandingSection.js
@@ -17,7 +17,7 @@ const LandingSection = () => {
     
     return (
         <div className='landing-section'>
-            <div className='heading' data-aos="fade-up" data-aos-duration="2000" data-aos-delay="500">
+            <div className='heading' data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500">
                 <h1 className='heading-primary'>The Electronics Club</h1>
                 <h3 className='heading-secondary'>Vellore Institute of Technology</h3>
                 <Link to='/events'><button className='heading-btn'>Upcoming Events <FontAwesomeIcon className='icon' icon={faChevronRight} /></button></Link>
