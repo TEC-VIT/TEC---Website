0b0b8bfb341e17af42051d7438631a79729e1359
unknown <yashwanthkolli3271@gmail.com>
2021-02-16 22:14:43 +0530

    Errors

--- src/Components/ProjectCard/ProjectCard.js
+++ src/Components/ProjectCard/ProjectCard.js
@@ -12,10 +12,12 @@ const ProjectCard = (props) => {
                 <div className='flip-card-front'>
                     <picture className='card-background'>
                         <img className='project-img' src={props.poster} alt={'project poster'} />
-                    </picture>                    
-                    <h1 className='project-name'>{props.name} </h1>
-                    <h3 className='project-intro'>{props.intro}</h3>
-                    <h4 className='project-date'>Date Created: {props.date}</h4>
+                    </picture>
+                    <div className='project-details'>                  
+                        <h1 className='project-name'>{props.name} </h1>
+                        <h3 className='project-intro'>{props.intro}</h3>
+                        <h4 className='project-date'>Date Created: {props.date}</h4>
+                    </div>
                 </div>
                 <div className='flip-card-back'>
                     <p className='project-description'>{props.description}</p>
