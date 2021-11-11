400233d7ff6820be1d97e6395a0983005add2c44
unknown <yashwanthkolli3271@gmail.com>
2021-11-12 00:28:55 +0530

    Upcomming sessions- IoTautomata

--- src/Assets/Data/UpcomingEvents.js
+++ src/Assets/Data/UpcomingEvents.js
@@ -1,4 +1,4 @@
-import tectronicsPoster from '../UpcomingEvents/TECtronics.webp'
+import iotautomataPoster from '../UpcomingEvents/IoTautomata.webp'
 
 // {
 //     id: 1,
@@ -11,10 +11,10 @@ import tectronicsPoster from '../UpcomingEvents/TECtronics.webp'
 
 const UpcomingEvents = [{
         id: 1,
-        event: "TECtronics",
-        time: "11-11-2021 7:30PM",
-        poster: tectronicsPoster,
-        details: <div>Ever wondered if you could make a home automation system or a robotic car? <br /><br />TEC-VIT brings to you TECtronics, a beginner-friendly interactive session which introduces you to Arduino and to the world of electronics as a whole!<br /><br />Arduino is an open source electronics platform which has been the brain of thousands of projects, from everyday objects to complex scientific instruments. You don’t want to miss out on this interactive and informative session! <br /><br />Register now in <a href="https://vtop.vit.ac.in/vtop/initialProcess" rel="noreferrer" target="_blank">VTOP</a></div>
+        event: "IoTautomata",
+        time: "12-11-2021 6:00PM",
+        poster: iotautomataPoster,
+        details: <div>Ever wondered if you could control the lights at home with a phone? If your car indicators could turn on and off automatically on the road? Life would become so much easier if your refrigerator could tell you that you were running short on groceries! But how? <br /><br />IoT is the answer to all your questions. <br /><br />At IoTautomata, a hands-on introductory session to the world of IoT, you will learn the fundamentals of building an IoT system and gain valuable experience of working with beginner-friendly IoT tools such as Node-RED. <br /><br />Join us at 6:00 PM on November 12th to know more about IoT and Automation! Link in Bio!<br /><br />Register now in <a href="https://vtop.vit.ac.in/vtop/initialProcess" rel="noreferrer" target="_blank">VTOP</a></div>
     }
 ]
 
