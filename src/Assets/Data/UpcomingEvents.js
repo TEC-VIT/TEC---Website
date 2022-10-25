7bb3bd1e34b7409aebad22df60e063e91ad3d04c
unknown <yashwanthkolli3271@gmail.com>
2022-10-25 18:03:38 +0530

    Sensored removed from Upcoming events

--- src/Assets/Data/UpcomingEvents.js
+++ src/Assets/Data/UpcomingEvents.js
@@ -1,4 +1,4 @@
-// import Poster from '../UpcomingEvents/QW.webp'
+import Poster from '../UpcomingEvents/sensored.jpg'
 
 // {
 //     id: 1,
@@ -10,6 +10,15 @@
 //     <div>....<br /><br />....<br /><br />Register now on <a href='https://vtop.vit.ac.in/vtop/initialProcess' target='_blank' rel='noreferrer'>VTOP</a>!</div>
 // }
 
-const UpcomingEvents = []
+const UpcomingEvents = [
+    // {
+    //     id: 1,
+    //     event: "Sensored",
+    //     time: "30-11-2020",
+    //     poster: Poster,
+    //     // registration: 'https://docs.google.com/forms/d/e/1FAIpQLScvdOGqVmcsYqPXd8WHmfMPZYKjC4UyTiLRVvN4VnpYKKDzLQ/viewform?embedded=true',
+    //     details: <div>Pull out your calendars and mark the date! We at TEC are thrilled to present to you the sixth edition of Sensored, our two-day electronics-based hands-on workshop. Join us as we take you through the fascinating world of smart devices and sensor interfacing!<br /><br />Register now on <a href='https://gravitas.vit.ac.in/events' target='_blank' rel='noreferrer'>Gravitas Portal</a>!</div>
+    // }
+]
 
 export default UpcomingEvents
